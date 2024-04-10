6a88e14dc7f3c02846daf44bc00be6eb4c53b847
Gnuhnat <116536811+AceGnuh@users.noreply.github.com>
2024-04-10 13:29:09 +0700

    fix bug order in menu

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -21,7 +21,6 @@ let dropdowns = document.querySelectorAll('.dropdown');
 let dropdownItems = document.querySelectorAll('.dropdown-item');
 
 document.addEventListener('DOMContentLoaded', function() {
-
     //handle hover event to expand dropdown menu
     dropdowns.forEach(function(dropdown) {
         dropdown.addEventListener('mouseenter', function() {
@@ -39,8 +38,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 dropdownMenu.classList.remove('show');
             }
         });
-
-        
     });
 
     dropdownItems.forEach(function(dropdownItem) {
@@ -51,95 +48,41 @@ document.addEventListener('DOMContentLoaded', function() {
 
     //handle resize event on window
     window.onresize = function() {
-        console.log("resize > 1600");
-
-
-        if(window.innerWidth > 1500){
-            
+        if (window.innerWidth > 1500 || window.innerWidth < 992) {  
             containerList.forEach(function(containerItem) {
                 containerItem.classList.remove('container');
                 containerItem.classList.add('container-fluid');
-                
             })
-
         }
-        else{
+        else {
             containerList.forEach(function(containerItem) {
                 containerItem.classList.remove('container-fluid');
                 containerItem.classList.add('container');
             })
         }
 
-        if(window.innerWidth < 992){  
+        if (window.innerWidth < 992){  
             btnContact.style.backgroundColor = secondaryColor;
             btnContact.style.color = "#fff";
         }
-        else{
+        else {
             btnContact.style.backgroundColor = "#fff";
             btnContact.style.color = primaryColor;
         }
-
-
-
     }
 
     handleSizeWindow();
 
 });
 
-// navLinkItemList.forEach(function(navItem) {
-//     navItem.addEventListener('click', function() {
-//         // this.nextElementSibling.classList.add('show');
-//         // console.log(this.nextElementSibling.classList);
-//     })
-// })
-
-
-// menuItemList.forEach(function(menuItem) {
-//     menuItem.addEventListener('mouseover', function() {
-
-//         let siblingElement = this.previousElementSibling;
-//         siblingElement.style.cssText = `color : ${secondaryColor}!important`;
-//         //this.click();
-//         navLinkItemList.forEach(function(navItem) {
-//             navItem.style.cssText = `color : #fff`;
-//         })
-//     });
-
-//     menuItem.addEventListener('mouseout', function() {
-
-//         // let siblingElement = this.previousElementSibling;
-//         this.style.cssText = `color : #fff!important`;
-//     });
-// });
-
-// navLinkItemList.forEach(function(menuItem) {
-//     menuItem.addEventListener('click', function() {
-//         this.siblingElement.style.cssText = `color : #fff!important`;
-//     })
-// })
-
-//handle mouseover event on nav link in menu
-// menuItemList.forEach(function(menuItem) {
-//     menuItem.addEventListener('mouseout', function() {
-//         // if(this.classList.contains('show')){
-//             let siblingElement = this.previousElementSibling;
-//             siblingElement.style.cssText = `color : #fff!important`;
-//         // }
-//     });
-// });
-
 function handleSizeWindow() {
-    if(window.innerWidth > 1500){
-        
+    if (window.innerWidth > 1500 || window.innerWidth < 992){
         containerList.forEach(function(containerItem) {
             containerItem.classList.remove('container');
             containerItem.classList.add('container-fluid');
-            
         })
-
     }
-    else{
+    else {
         containerList.forEach(function(containerItem) {
             containerItem.classList.remove('container-fluid');
             containerItem.classList.add('container');
@@ -147,60 +90,48 @@ function handleSizeWindow() {
     }
 }
 
-
 //handle click event on main area
 mainArea.addEventListener('click', function() {
-    if(window.innerWidth < 992){
+    if (window.innerWidth < 992){
         btnToggle.style.backgroundColor = primaryColor;
 
         if(navbarCollapse.classList.contains('show')){
-            //navbarCollapse.classList.remove('show');
             btnToggleIcon.click();
-            // btnToggle.style.color = "#fff";
             btnToggle.style.backgroundColor = primaryColor;
         }
     }
-    else{
+    else {
         btnContact.style.backgroundColor = "#fff";
         btnContact.style.color = primaryColor;
     }
 
     navLinkItemList.forEach(function(navItem) {
-        if(navItem.nextElementSibling){
-            if(navItem.nextElementSibling.classList.contains('show')){
+        if (navItem.nextElementSibling){
+            if (navItem.nextElementSibling.classList.contains('show')){
                 navItem.nextElementSibling.classList.remove('show');
             }
         }
     })
 });
 
-
 //handle click event on contact button
 btnContact.addEventListener('click', function() {
-    // if(!isClickBtnContact){
-        this.style.backgroundColor = secondaryColor;
-        this.style.color = "#fff";
-        // isClickBtnContact = true;
-    // }
-    // else{
-    //     this.style.backgroundColor = "#fff";
-    //     this.style.color = "#000";
-    //     isClickBtnContact = false;
-    // }
+    this.style.backgroundColor = secondaryColor;
+    this.style.color = "#fff";
 });
 
 //handle toggle button is rotated 90 degree
 let isClick = false;
 btnToggle.addEventListener('click', function() {
     
-    if(!isClick){
+    if (!isClick){
         this.style.transform = 'rotate(90deg)';
         btnToggle.style.backgroundColor = "rgb(55 65 81)";
 
         //toggle button is clicked
         isClick = true;
     }
-    else{
+    else {
         this.style.transform = 'rotate(0deg)';
         btnToggle.style.backgroundColor = "rgb(55 65 81)"
 
